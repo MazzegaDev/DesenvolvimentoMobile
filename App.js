f5c1699a6fad1dd0037d763a69c38ae3fe401f87
Mazzega_Dev <mazzega_dev@unoeste.edu.br>
2026-02-25 20:21:05 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [open, setOpen] = useState(true);
+
+	const handleOpen = () => {
+		setOpen(false);
+	};
+
+
+
+	return (
+		<View style={styles.container}>
+			{!open ? (
+				<View>
+					<Image source={require("./assets/biscoitoQuebrado.png")}></Image>
+					
+				</View>
+			) : (
+				<Image source={require("./assets/biscoito.png")}></Image>
+			)}
+
+			<Text onPress={handleOpen}>Quebrar biscoito</Text>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
