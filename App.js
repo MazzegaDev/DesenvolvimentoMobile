25ac2d029cbfb735747dd0edcada512036bec9ce
MazzegaDev <mazzega_dev@unoeste.edu.br>
2026-03-11 20:26:08 -0300

    Exemplo de flexbox

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={styles.caixa1}>
+				<Text style={styles.text}>Caixa 1</Text>
+			</View>
+
+			<View style={styles.caixa2}>
+				<Text style={styles.text}>Caixa 2</Text>
+			</View>
+
+			<View style={styles.caixa3}>
+				<Text style={styles.text}>Caixa 3</Text>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "center",
+    alignItems: "center",
+    gap: '2%',
+		width: "100%",
+		height: "100%",
+	},
+	caixa1: {
+		backgroundColor: "blue",
+		width: "30%",
+		height: "30%",
+    bottom: "20%"
+	},
+	caixa2: {
+		backgroundColor: "red",
+		width: "30%",
+		height: "30%",
+	},
+	caixa3: {
+		backgroundColor: "green",
+		width: "30%",
+		height: "30%",
+    top: "20%"
+	},
+  text:{
+    top: "50%",
+    alignSelf: "center"
+  }
 });
